f645a0460cdc9874d18ad2610cc24f5c8c4a26ca
Jose Armando Sandoval Santana <sandovalsantanajosearmando@gmail.com>
2025-07-07 10:44:17 -0600

    cambios en el controlador stream

--- src/main/java/com/agromatik/cloud/infrastructure/web/SensorController.java
+++ src/main/java/com/agromatik/cloud/infrastructure/web/SensorController.java
@@ -2,6 +2,7 @@ package com.agromatik.cloud.infrastructure.web;
 
 import com.agromatik.cloud.application.dto.SensorDataDTO;
 import com.agromatik.cloud.application.port.in.SensorDataUseCase;
+import com.agromatik.cloud.domain.model.SensorData;
 import com.agromatik.cloud.infrastructure.persistence.repository.SpringDataSensorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -32,17 +33,40 @@ public class SensorController {
                                       @RequestParam(defaultValue = "10") int size) {
         return useCase.getAll(PageRequest.of(page, size));
     }
-
     @GetMapping(path = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<SensorDataDTO> streamSensorData() {
         return Flux.interval(Duration.ofSeconds(1))
                 .flatMap(sequence -> Mono.fromCallable(() -> repository.findTopByOrderByTimestampDesc()))
                 .map(optional -> optional.orElse(null))
                 .filter(entity -> entity != null)
-                .map(entity -> {
-                    SensorDataDTO dto = new SensorDataDTO();
-                    BeanUtils.copyProperties(entity, dto);
-                    return dto;
-                });
+                .map(entity -> mapEntityToDto(entity));
     }
+
+    private SensorDataDTO mapEntityToDto(SensorData entity) {
+        SensorDataDTO.GeneralData general = SensorDataDTO.GeneralData.builder()
+                .temperature(entity.getGeneralTemperature())
+                .humidity(entity.getGeneralHumidity())
+                .build();
+
+        SensorDataDTO.PlantsData plants = SensorDataDTO.PlantsData.builder()
+                .temperature(entity.getPlantsTemperature())
+                .humidity(entity.getPlantsHumidity())
+                .soilMoisture(entity.getPlantsSoilMoisture())
+                .build();
+
+        SensorDataDTO.WaterData water = SensorDataDTO.WaterData.builder()
+                .soilMoisture(entity.getWaterSoilMoisture())
+                .pH(entity.getWaterPH())
+                .TDS(entity.getWaterTDS1())
+                .build();
+
+        return SensorDataDTO.builder()
+                .general(general)
+                .plants(plants)
+                .water(water)
+                .timestamp(entity.getTimestamp())
+                .build();
+    }
+
+
 }
