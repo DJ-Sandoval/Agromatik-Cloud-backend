a807ae320c7b14572907d8e4591f8b5b61f37c2b
Jose Armando Sandoval Santana <sandovalsantanajosearmando@gmail.com>
2025-07-08 14:30:58 -0600

    cambios en el servicio

--- src/main/java/com/agromatik/cloud/infrastructure/web/SensorController.java
+++ src/main/java/com/agromatik/cloud/infrastructure/web/SensorController.java
@@ -14,6 +14,7 @@ import reactor.core.publisher.Mono;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 import org.springframework.beans.BeanUtils;
@@ -73,6 +74,7 @@ public class SensorController {
                 .waterSoilMoisture(entity.getWaterSoilMoisture())
                 .waterPH(entity.getWaterPH())
                 .waterTDS(entity.getWaterTDS())
+                .timestamp(LocalDateTime.now())
                 .build();
     }
 
