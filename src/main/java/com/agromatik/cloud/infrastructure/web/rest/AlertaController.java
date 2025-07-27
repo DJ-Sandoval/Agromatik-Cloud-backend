5338fd586800ef6c6ab2968923b87d53da2a76af
Jose Armando Sandoval Santana <sandovalsantanajosearmando@gmail.com>
2025-07-27 00:58:38 -0600

    corrigiendo error en ruta de controlador de alertas

--- src/main/java/com/agromatik/cloud/infrastructure/web/rest/AlertaController.java
+++ src/main/java/com/agromatik/cloud/infrastructure/web/rest/AlertaController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/agromatrik/alertas")
+@RequestMapping("/api/v1/agromatik/alertas")
 @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 @RequiredArgsConstructor
 public class AlertaController {
