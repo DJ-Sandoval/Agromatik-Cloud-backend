e6ce827181aa9642f9972c601f0d86b24005eaf6
Jose Armando Sandoval Santana <sandovalsantanajosearmando@gmail.com>
2025-07-28 03:46:48 -0600

    arreglando error de mongo

--- src/main/java/com/agromatik/cloud/infrastructure/config/database/DatabaseConfig.java
+++ src/main/java/com/agromatik/cloud/infrastructure/config/database/DatabaseConfig.java
@@ -2,10 +2,9 @@ package com.agromatik.cloud.infrastructure.config.database;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
-@EnableMongoRepositories(basePackages = "com.agromatik.cloud.infrastructure.mongo.repository")
+
 @EnableJpaRepositories(basePackages = "com.agromatik.cloud.infrastructure.mysql.repository")
 public class DatabaseConfig {
 }
